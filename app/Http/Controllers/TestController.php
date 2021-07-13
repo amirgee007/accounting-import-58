8bdc7ab61e59b62cf6808076a16a71041e617534
Amir <Amirgee007@yahoo.com>
2021-07-14 01:03:50 +0500

    done

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -16,7 +16,7 @@ class TestController extends Controller
         ini_set('max_execution_time', 30000000); //300 seconds = 5 minutes
 
 
-        $content = 'Hi, Your images has been processed';
+        $content = 'Hi, Your images has been processed and API is working fine.';
 
         $email = Setting::where('key','adminEmail')->first();
 
